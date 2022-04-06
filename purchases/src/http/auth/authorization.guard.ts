4aeda405bb7d119c74a28192c69c318d381872e3
João Vítor <joaovsoutosantos@gmail.com>
2022-04-05 23:10:50 -0300

    :tada: Initial commit

---
+++ purchases/src/http/auth/authorization.guard.ts
@@ -0,0 +1,20 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import jwt from 'express-jwt';
+import { expressJwtSecret } from 'jwks-rsa';
+import { promisify } from 'util';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  private AUTH0_AUDIENCE: string;
+  private AUTH0_DOMAIN: string;
+
+  constructor(private configService: ConfigService) {
+    this.AUTH0_AUDIENCE = this.configService.get('AUTH0_AUDIENCE') ?? '';
+    this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? '';
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    return true;
+  }
+}
