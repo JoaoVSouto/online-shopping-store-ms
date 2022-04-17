2ddbb2a2778d0f9e06418fc11e7703d9bbc4bc34
João Vítor <joaovsoutosantos@gmail.com>
2022-04-17 10:06:15 -0300

    feat(purchases): create endpoint for creating purchases

---
+++ purchases/src/services/customers.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '../database/prisma/prisma.service';
+
+type CreateCustomerParams = {
+  authUserId: string;
+};
+
+@Injectable()
+export class CustomersService {
+  constructor(private prisma: PrismaService) {}
+
+  getCustomerByAuthUserId(authUserId: string) {
+    return this.prisma.customer.findUnique({ where: { authUserId } });
+  }
+
+  createCustomer({ authUserId }: CreateCustomerParams) {
+    return this.prisma.customer.create({ data: { authUserId } });
+  }
+}
